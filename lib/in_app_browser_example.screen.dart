0c8349683beb472831474755bf20d302baf7dcab
Brendan Mullins <brendan.mullins@9am.health>
2024-09-24 11:18:33 +0200

    update agent

--- lib/in_app_browser_example.screen.dart
+++ lib/in_app_browser_example.screen.dart
@@ -130,7 +130,7 @@ class _InAppBrowserExampleScreenState extends State<InAppBrowserExampleScreen> {
                           useOnDownloadStart: true,
                           mediaPlaybackRequiresUserGesture: false,
                           javaScriptCanOpenWindowsAutomatically: true,
-                          applicationNameForUserAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 InAppTC/1.0",
+                          applicationNameForUserAgent: "InAppTC/1.0",
                         ),
                       ),
                     );
